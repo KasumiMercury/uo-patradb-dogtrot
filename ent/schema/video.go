03c93537923161154c46d05e10567caa5c5128c7
KasumiMercury <naoki_shira@outlook.jp>
2023-12-03 23:11:44 +0900

    :construction: fix edge / for fail to generate

--- ent/schema/video.go
+++ ent/schema/video.go
@@ -44,8 +44,8 @@ func (Video) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.To("descriptions", Description.Type).Unique().StorageKey(edge.Column("video_id")),
 		edge.To("channel", Channel.Type),
-		edge.To("video_play_range", Video_play_range.Type).StorageKey(edge.Column("video_id")),
-		edge.To("video_disallow_range", Video_disallow_range.Type).StorageKey(edge.Column("video_id")),
-		edge.To("video_title_change", Video_title_change.Type).StorageKey(edge.Column("video_id")),
+		edge.To("video_play_ranges", Video_play_range.Type).StorageKey(edge.Column("video_id")),
+		edge.To("video_disallow_ranges", Video_disallow_range.Type).StorageKey(edge.Column("video_id")),
+		edge.To("video_title_changes", Video_title_change.Type).StorageKey(edge.Column("video_id")),
 	}
 }

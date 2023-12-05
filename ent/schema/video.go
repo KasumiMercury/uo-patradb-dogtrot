780e3149f1dff2c02bdfe3c75fc917bdb3a7208c
KasumiMercury <naoki_shira@outlook.jp>
2023-12-05 23:19:41 +0900

    :construction: define gRPC annotations

--- ent/schema/video.go
+++ ent/schema/video.go
@@ -1,7 +1,9 @@
 package schema
 
 import (
+	"entgo.io/contrib/entproto"
 	"entgo.io/ent"
+	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 	"github.com/KasumiMercury/uo-patradb-dogtrot/ent/schema/pulid"
@@ -23,19 +25,19 @@ func (Video) Mixin() []ent.Mixin {
 // Fields of the Video.
 func (Video) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("video_id").Unique(),
-		field.String("title"),
+		field.String("video_id").Unique().Annotations(entproto.Field(2)),
+		field.String("title").Annotations(entproto.Field(3)),
 		field.String("normalized_title"),
-		field.Int("duration_seconds"),
-		field.Bool("is_collaboration"),
-		field.String("status"),
+		field.Int("duration_seconds").Annotations(entproto.Field(4)),
+		field.Bool("is_collaboration").Annotations(entproto.Field(5)),
+		field.String("status").Annotations(entproto.Field(6)),
 		field.String("chat_id").Optional(),
-		field.Bool("has_time_range").Default(false),
-		field.Time("scheduled_at").Optional(),
-		field.Time("actual_start_at").Optional(),
-		field.Time("published_at"),
+		field.Bool("has_time_range").Default(false).Annotations(entproto.Field(7)),
+		field.Time("scheduled_at").Optional().Annotations(entproto.Field(8)),
+		field.Time("actual_start_at").Optional().Annotations(entproto.Field(9)),
+		field.Time("published_at").Annotations(entproto.Field(10)),
 		field.Time("created_at").Default(func() time.Time { return time.Now() }),
-		field.Time("updated_at").Default(func() time.Time { return time.Now() }).UpdateDefault(func() time.Time { return time.Now() }),
+		field.Time("updated_at").Default(func() time.Time { return time.Now() }).UpdateDefault(func() time.Time { return time.Now() }).Annotations(entproto.Field(11)),
 	}
 }
 
@@ -48,5 +50,12 @@ func (Video) Edges() []ent.Edge {
 		edge.To("video_disallow_ranges", Video_disallow_range.Type).StorageKey(edge.Column("video_id")),
 		edge.To("video_title_changes", Video_title_change.Type).StorageKey(edge.Column("video_id")),
 		edge.To("Pat_chats", Pat_chat.Type).StorageKey(edge.Column("video_id")),
+	}
+}
+
+// Annotations of the Video.
+func (Video) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entproto.Message(),
 	}
 }
--- go.mod
+++ go.mod
@@ -8,15 +8,26 @@ require (
 )
 
 require (
-	ariga.io/atlas v0.14.1-0.20230918065911-83ad451a4935 // indirect
-	github.com/agext/levenshtein v1.2.1 // indirect
+	ariga.io/atlas v0.15.0 // indirect
+	entgo.io/contrib v0.4.5 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/bufbuild/protocompile v0.7.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
-	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/zclconf/go-cty v1.8.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.19.1 // indirect
+	github.com/jhump/protoreflect v1.15.3 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/zclconf/go-cty v1.14.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.16.0 // indirect
+	google.golang.org/protobuf v1.31.1-0.20231027082548-f4a6c1f6e5c1 // indirect
 )

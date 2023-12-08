b350762e40e4e778cc78b273a39c92c8e4cacfc2
KasumiMercury <naoki_shira@outlook.jp>
2023-12-08 17:21:20 +0900

    :construction: create the server

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"github.com/KasumiMercury/uo-patradb-dogtrot/ent"
+	"github.com/KasumiMercury/uo-patradb-dogtrot/ent/proto/entpb"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fprintf, err := fmt.Fprintf(w, "Hello, World")
-	if err != nil {
-		return
+func loadEnv() {
+	_, err := os.Stat(".env")
+	if err == nil {
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("failed to load .env file: %v", err)
+		}
 	}
-	fmt.Println(fprintf)
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	loadEnv()
 
-	// Start the server on port 8080
-	err := http.ListenAndServe(":8080", nil)
+	usr := os.Getenv("MYSQL_USER")
+	pwd := os.Getenv("MYSQL_PASSWORD")
+	hst := os.Getenv("MYSQL_HOST")
+	prt := os.Getenv("MYSQL_PORT")
+	db := os.Getenv("MYSQL_DATABASE")
+	client, err := ent.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", usr, pwd, hst, prt, db))
 	if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+		log.Fatalf("failed to connect to mysql: %v", err)
+	}
+	defer func(client *ent.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatalf("failed to close mysql connection: %v", err)
+		}
+	}(client)
+
+	server := grpc.NewServer()
+
+	// Initialize ent services
+	vs := entpb.NewVideoService(client)
+	ds := entpb.NewDescriptionService(client)
+	pds := entpb.NewPeriodicDescriptionTemplateService(client)
+	cds := entpb.NewCategoryDescriptionTemplateService(client)
+	cd := entpb.NewChannelService(client)
+	pcs := entpb.NewPatChatService(client)
+
+	// Register ent services
+	entpb.RegisterVideoServiceServer(server, vs)
+	entpb.RegisterDescriptionServiceServer(server, ds)
+	entpb.RegisterPeriodicDescriptionTemplateServiceServer(server, pds)
+	entpb.RegisterCategoryDescriptionTemplateServiceServer(server, cds)
+	entpb.RegisterChannelServiceServer(server, cd)
+	entpb.RegisterPatChatServiceServer(server, pcs)
+
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }

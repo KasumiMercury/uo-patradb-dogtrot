aca54eb5a1374af15fce78014e1d15a4d272649e
KasumiMercury <naoki_shira@outlook.jp>
2023-12-20 17:30:12 +0900

    :construction: recreate

--- ent/migrate/migrations/integrate/20231211122500_extend_description.sql
+++
@@ -1,4 +0,0 @@
--- Modify "description_changes" table
-ALTER TABLE `description_changes` MODIFY COLUMN `raw` varchar(5000) NOT NULL;
--- Modify "descriptions" table
-ALTER TABLE `descriptions` MODIFY COLUMN `raw` varchar(5000) NOT NULL;
